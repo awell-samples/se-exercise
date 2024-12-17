3a6eda4a9e2473b38d68e7a1ee7374c92897b0ab
Nick Hellemans <nick@awellhealth.com>
2024-12-17 12:24:19 +0100

    chore(): contextualize constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,8 @@
+// Also see https://careops.sandbox.awellhealth.com/settings/identifiers
 export const ELATION_IDENTIFIER_SYSTEM = 'https://www.elationhealth.com/'
+
+// Also see https://studio.sandbox.awellhealth.com/pathways/9yXrOSSR43f7
 export const BREAST_CANCER_SCREENING_PATHWAY_DEFINITION_ID = '9yXrOSSR43f7'
+
+// Also see https://studio.sandbox.awellhealth.com/pathways/9yXrOSSR43f7/settings/general
 export const LAST_BREAST_SCREENING_DATE_DATA_POINT_DEFINITION_ID = 'QbzLyxg6XQB9'
